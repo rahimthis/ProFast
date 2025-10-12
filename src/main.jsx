fe862ffdc42533763a73fbd3965b5cf79d5ce622
rahimthis <info.abdurrahim12@gmail.com>
2025-10-12 14:50:30 +0600

    navbar logo add

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { RouterProvider } from 'react-router'
-import { router } from './router/router.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider } from "react-router";
+import { router } from "./router/router.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-  <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <div className="font-urbanist max-w-7xl mx-auto">
+      <RouterProvider router={router} />
+    </div>
+  </StrictMode>
+);
--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router";
+import ProFastLogo from "../ProFastLogo/ProFastLogo";
 
 const Navbar = () => {
   const navItems = (
@@ -39,7 +40,9 @@ const Navbar = () => {
             {navItems}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">
+        <ProFastLogo/>
+        </a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{navItems}</ul>
